756492d990a79a7f5e88bd06a4915aa401fb353c
devenjain <digujain2001@gmail.com>
2021-08-29 18:43:34 +0530

    implemented for parameter and code cleanup

--- src/main/java/com/controller/MainController.java
+++ src/main/java/com/controller/MainController.java
@@ -6,6 +6,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -31,9 +32,6 @@ public class MainController {
 		double userLatitude = userLocationBean.getLatitude();
 		double userLongitude = userLocationBean.getLongitude();
 		
-		System.out.println("user - latitude : " + userLatitude);
-		System.out.println("user - longitude : " + userLongitude);
-		
 		String url = "https://boni.co.in/v1/businesses";
 	
 		ObjectMapper objMapper = new ObjectMapper();
@@ -42,7 +40,7 @@ public class MainController {
 		
 		ResponseEntity<String> res = restTemplate.exchange (builder.buildAndExpand ().toUri(), HttpMethod.GET, null, String.class);
 		
-		System.out.println(res);
+//		System.out.println(res);
 		
 		MainDao dao = new MainDao();
 		
@@ -50,11 +48,6 @@ public class MainController {
 		
 		try {
 			buisnessBean = objMapper.readValue(res.getBody(), BuisnessBean[].class);
-			
-			System.out.println();
-			System.out.println();
-			System.out.println("bus - lat : " + buisnessBean[2].getLatitude());
-			System.out.println("bus - lon : " + buisnessBean[2].getLongitude());
 			
 			for (int i = 0; i < buisnessBean.length; i++) {
 				
@@ -80,11 +73,10 @@ public class MainController {
 		
 	}
 	
-//	@PostMapping(value = "/nearMe", consumes = MediaType.APPLICATION_JSON_VALUE)
-//	public BuisnessBean[] findBuisness(@RequestParam Double userLatitude,@RequestParam Double userLongitude){
-//		
-////		double userLatitude = userLocation.getLatitude();
-////		double userLongitude = userLocation.getLongitude();
+	
+	// by using parameters
+//	@PostMapping(value = "/nearMe")
+//	public BuisnessBean[] findBuisness(Double userLatitude, Double userLongitude){
 //		
 //		System.out.println("user - latitude : " + userLatitude);
 //		System.out.println("user - longitude : " + userLongitude);
